df8b1429c5d2e3d316402f4522e572a2f7b9ac73
TomlyYang <529089975@qq.com>
2018-10-10 10:11:25 +0000

    Update display.c

--- display.c
+++ display.c
@@ -2,8 +2,11 @@
 unsigned char drelay[14]={0xfc,0x60,0xda,0xf2;0x66,0xb6,0xbe,0xe0,0xfe,0xf6,
 	0xee,0x3e,0x1a,0x7a};//段码，显示0~d的值
 unsigned char wdelay[4]={0x08;0x04;0x02;0x01};//位码
-void exchange(unsigned char source)
-unsigned char display(unsigned char a1,unsigned char a2,unsigned char a3)
+void exchange(unsigned char source,unsigned char bwsource)
+{
+	
+}
+unsigned char display(unsigned char a1,unsigned char a2,unsigned char a3,unsigned char a4)//传递预显示数
 {
 	
 	TMOD=0x01;
@@ -11,5 +14,16 @@ unsigned char display(unsigned char a1,unsigned char a2,unsigned char a3)
 	TL0=0xf6;
 	IE=0x82;
 	TR0=1;
+bit exch;
+unsigned char nexch,bwdelay,i;
+i=8;
+while(i--)
+{
+nexch=delay[a1];
+bwdelay=wdelay[0];
+
+}
+
+
 	
 }
