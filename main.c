c893ac0ffa78554036282d9d5818058859b8ec41
TomlyYang <529089975@qq.com>
2018-10-11 02:14:01 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include"reg52.h"
-sbit DS=P0^0;//display dat
+#include"stdio.h"
+sbit DS=P0^0;//?????
 sbit CLK=P0^1;//Input and RL clock
 sbit ST=P0^2;//display clock
-DS=0;
-CLK=0;
-ST=0;
+/*	TMOD=0x01;
+	TH0=0xff;//timer 100us
+	TL0=0xf6;
+	IE=0x82;
+	TR0=1;
+*/
+
 void main()
 {
-	
+	DS=0;
+  CLK=0;
+  ST=0;
 }
